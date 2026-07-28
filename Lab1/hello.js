334e61c8615342a5c28b9801e9686f8a53eef769
amit <giri10112006@gmail.com>
2026-07-28 14:35:49 +0530

    event

---
+++ Lab1/hello.js
@@ -0,0 +1,14 @@
+function sayhello (name) {
+    console.log(`welcome to ${name}`);
+}
+const sayhi = (name) => {
+    console.log(`welcome to ${name}`);
+};
+
+
+function sum(a, b) {
+    return a + b;
+}
+sayhello('amit giri');
+sayhi('jon bnega don');
+console.log('sum of 2 and 3 is', sum(2, 3));
---
+++ q1.js
@@ -0,0 +1,41 @@
+const towords =(digit) => {
+    switch(digit) {
+        case 0:
+            return "zero";
+        case 1:
+            return "one";
+        case 2:
+            return "two";
+        case 3:
+            return "three";
+        case 4:
+            return "four";
+        case 5:
+            return "five";
+        case 6:
+            return "six";
+        case 7:
+            return "seven";
+        case 8:
+            return "eight";
+        case 9:
+            return "nine";
+        default:
+            return "invalid input";
+    }
+};
+
+//console.log(towords(5));
+//console.log(towords(10));
+//console.log(towords(3));
+//console.log(towords(-1));
+
+
+const rollnum ="123456789";
+const digit = String(rollnum).split("");
+console.log(digit);
+
+
+digit.forEach((d) => {
+    inword += " " + towords(Number(d));
+});
